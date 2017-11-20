1f9c64f511ab6e8cf75c54ef1fd23fe09501b01a
unknown <19kennit@waipahuh.k12.hi.us>
2017-11-19 21:12:49 -1000

    did 4 operations on 3 variable numbers

---
+++ assignment.js
@@ -0,0 +1,13 @@
+var x = 100
+var y = 420
+var z = 1231
+
+function Calculator(num1, num2, num3) {
+	return num1 * num1 + num3 - num2 / num1;
+}
+
+var output = Calculator(x,y,z);
+
+console.log(output);
+
+/* I didn't know how to name the function. But it functions as a calculator. */
