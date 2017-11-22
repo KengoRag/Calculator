9f4e65eb90adc7adf4a8e19ec77d98dabd7fab46
unknown <19kennit@waipahuh.k12.hi.us>
2017-11-21 20:05:23 -1000

    finished

--- assignment.js
+++ assignment.js
@@ -1,4 +1,7 @@
-var x = 100
+/*
+This is my calculator without conditional statements
+
+ var x = 100
 var y = 420
 var z = 1231
 
@@ -8,6 +11,28 @@ function Calculator(num1, num2, num3) {
 
 var output = Calculator(x,y,z);
 
-console.log(output);
+console.log(output); */
 
-/* I didn't know how to name the function. But it functions as a calculator. */
+var output; 
+var gg = "For 'x' your options are either to add, subtract, multiply, or divide with the 2 numbers 'y' and 'z'.";
+function calculator(x, y, z) {
+	if (x == "add") {
+		output = y + z;
+
+	} else if (x == "subtract") {
+		output= y  - z; 
+
+	}else if (x == "multiply") {
+		output = y * z;
+
+	} else if (x == "divide") {
+		output = y / z;
+
+	} else {
+		output= "Only input 'add', 'subtract', 'multiply', and 'divide' for the operation. Thank you!."
+	}
+}
+
+calculator("divide", 24, 6);
+
+console.log(output);
